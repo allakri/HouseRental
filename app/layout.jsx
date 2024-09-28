886ecf56c11667b23a9e9558db1172f9b5191bbd
abhishek <raiabhishek3646@gamil.com>
2024-09-28 14:51:31 +0530

    all files are finalized

---
+++ app/layout.jsx
@@ -0,0 +1,33 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { GlobalProvider } from '@/context/GlobalContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '@/assets/styles/globals.css';
+import 'photoswipe/dist/photoswipe.css';
+
+export const metadata = {
+  title: 'PropertyPulse',
+  description: 'Find The Perfect Rental Property',
+  keywords: 'rental, property, real estate',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <GlobalProvider>
+        <html lang='en'>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </GlobalProvider>
+    </AuthProvider>
+  );
+};
+
+export default MainLayout;
---
+++ components/PropertyContactForm.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+import { useSession } from "next-auth/react";
+import { toast } from "react-toastify";
+import addMessage from "@/app/actions/addMessage";
+import SubmitMessageButton from "./SubmitMessageButton";
+
+const PropertyContactForm = ({ property }) => {
+  const { data: session } = useSession();
+
+  const [state, formAction] = useFormState(addMessage, {});
+
+  useEffect(() => {
+    if (state.error) toast.error(state.error);
+    if (state.submitted) toast.success("Message sent successfully");
+  }, [state]);
+
+  if (state.submitted) {
+    return (
+      <p className="text-green-500 mb-4">
+        Your message has been sent successfully
+      </p>
+    );
+  }
+
+  return (
+    session && (
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
+        <form action={formAction}>
+          <input
+            type="hidden"
+            id="property"
+            name="property"
+            defaultValue={property._id}
+          />
+          <input
+            type="hidden"
+            id="recipient"
+            name="recipient"
+            defaultValue={property.owner}
+          />
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Name:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="phone"
+            >
+              Phone:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="phone"
+              name="phone"
+              type="text"
+              placeholder="Enter your phone number"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="message"
+            >
+              Message:
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
+              id="message"
+              name="message"
+              placeholder="Enter your message"
+            ></textarea>
+          </div>
+          <div>
+            <SubmitMessageButton />
+          </div>
+        </form>
+      </div>
+    )
+  );
+};
+export default PropertyContactForm;
